db56666f201fb6a1605247204ae31842cc9302b9
Patrick Ruan <patrickruan@Patricks-MacBook-Air-2.local>
2024-02-24 13:55:06 -0600

    some styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from "react";
-import data from "./with_country_codes.json";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import data from "./map_files/with_country_codes.json";
+import { ZoomableGroup, ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 
 function App() {
   return (
     <div className="App">
-      <ComposableMap>
-        <Geographies geography={data}>
-        {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
-        }
-        </Geographies>
+      <ComposableMap
+        projection="geoEqualEarth"
+        projectionConfig={{
+          scale: 200,
+        }}>
+          <Geographies geography={data}>
+              {({ geographies }) => 
+                geographies.map((geo) => (
+                  <Geography key={geo.rsmKey} 
+                            geography={geo}
+                            style={{
+                              default: {
+                                fill: "#3A3B3C",
+                              },
+                              hover: {
+                                fill: "#F53",
+                              },
+                              pressed: {
+                                fill: "#E42",
+                              },
+                            }} />
+                ))
+              }
+            </Geographies>
       </ComposableMap>
     </div>
   );
